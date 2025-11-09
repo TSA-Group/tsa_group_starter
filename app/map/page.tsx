c64eefdb5d0a541057da4b8f7d4da9a2709a4f34
pradyunvishu-design <pradyunpoorna@gmail.com>
2025-11-09 12:27:24 -0600

    Update page.tsx

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,3 +1,15 @@
-https://www.google.com/maps/embed/v1/search
-  ?key= AIzaSyCvg7nk61C3TUhEQlPjbAqpyfJA9OVjC08
-  &q=record+stores+in+Seattle
+export default function Page() {
+  return (
+    <div style={{ width: '100%', height: '100vh' }}>
+      <iframe
+        width="100%"
+        height="100%"
+        style={{ border: 0 }}
+        loading="lazy"
+        allowFullScreen
+        referrerPolicy="no-referrer-when-downgrade"
+        src="https://www.google.com/maps/embed/v1/search?key=AIzaSyCvg7nk61C3TUhEQlPjbAqpyfJA9OVjC08&q=record+stores+in+Seattle"
+      ></iframe>
+    </div>
+  );
+}
