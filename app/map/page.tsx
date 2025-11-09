e411d377d09de90bf3b4a3734444f5a519bc4cb2
stopblocking80 <126114544+stopblocking80@users.noreply.github.com>
2025-11-09 12:49:47 -0600

    Update page.tsx

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,13 +1,3 @@
 export default function Page() {
-  return (
-    initMap();
-  );
-}
-let map: google.maps.Map;
-async function initMap(): Promise<void> {
-  const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
-  map = new Map(document.getElementById("map") as HTMLElement, {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
+  return <h1>Hello Next.js!</h1>
 }
