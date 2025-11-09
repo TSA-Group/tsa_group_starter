691f95b61c3e54143598b9db36a3ddc04c5e2b44
stopblocking80 <126114544+stopblocking80@users.noreply.github.com>
2025-11-09 12:33:46 -0600

    Update page.tsx

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,15 +1,13 @@
 export default function Page() {
   return (
-    <div style={{ width: '100%', height: '100vh' }}>
-      <iframe
-        width="100%"
-        height="100%"
-        style={{ border: 0 }}
-        loading="lazy"
-        allowFullScreen
-        referrerPolicy="no-referrer-when-downgrade"
-        src="https://www.google.com/maps/embed/v1/search?key=AIzaSyCvg7nk61C3TUhEQlPjbAqpyfJA9OVjC08&q=record+stores+in+Seattle"
-      ></iframe>
-    </div>
+    initMap();
   );
+}
+let map: google.maps.Map;
+async function initMap(): Promise<void> {
+  const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+  map = new Map(document.getElementById("map") as HTMLElement, {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
 }
