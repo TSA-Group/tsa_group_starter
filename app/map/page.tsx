f74acf5849681e957b6d7056f1da0b6e44af3624
stopblocking80 <126114544+stopblocking80@users.noreply.github.com>
2025-11-09 22:43:34 -0600

    Update page.tsx

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -12,7 +12,7 @@ import{
 export default function Page() {
   const position = {lat: 53.54, lng: 10};
   return (
-    <APIProvider apiKey = {"AIzaSyCvg7nk61C3TUhEQlPjbAqpyfJA9OVjC08"}>
+    <APIProvider apiKey = {"AIzaSyA1o7Vio2dHZqCPqC4suZ1cJMPg79G2XFc"}>
         <div style={{height: "100vh"}}>
           <Map zoom ={9} center = {position}></Map>
         </div>
