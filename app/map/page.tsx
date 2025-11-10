6d072f15e405cfe35a77a2c225357698fe825cd5
stopblocking80 <126114544+stopblocking80@users.noreply.github.com>
2025-11-09 22:41:29 -0600

    Update page.tsx

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,3 +1,21 @@
+"use client";
+
+import { useState } from "react";
+import{
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Pin,
+  InfoWindow,
+} from "@vis.gl/react-google-maps";
+
 export default function Page() {
-  return <h1>Hello Next.js!</h1>
+  const position = {lat: 53.54, lng: 10};
+  return (
+    <APIProvider apiKey = {"AIzaSyCvg7nk61C3TUhEQlPjbAqpyfJA9OVjC08"}>
+        <div style={{height: "100vh"}}>
+          <Map zoom ={9} center = {position}></Map>
+        </div>
+    </APIProvider>
+  );
 }
