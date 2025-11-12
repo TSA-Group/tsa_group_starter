1ab32c2624a458608a05f2d0c7360e59753b8992
stopblocking80 <126114544+stopblocking80@users.noreply.github.com>
2025-11-11 18:33:18 -0600

    Update page.tsx

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,18 +1,3 @@
-/*
- * Copyright 2024 Google LLC. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 import React, { useState, useEffect, useRef } from 'react';
 import { createRoot } from 'react-dom/client';
 import {
@@ -27,8 +12,6 @@ import {
   AdvancedMarkerRef
 } from '@vis.gl/react-google-maps';
 
-const API_KEY =
-  globalThis.GOOGLE_MAPS_API_KEY ?? ("AIzaSyA1o7Vio2dHZqCPqC4suZ1cJMPg79G2XFc");
 
 const App = () => {
   const [selectedPlace, setSelectedPlace] =
@@ -37,10 +20,10 @@ const App = () => {
 
   return (
     <APIProvider
-      apiKey={API_KEY}
+      apiKey={"AIzaSyA1o7Vio2dHZqCPqC4suZ1cJMPg79G2XFc"}
       solutionChannel='GMP_devsite_samples_v3_rgmautocomplete'>
       <Map
-        mapId={'bf51a910020fa25a'}
+        mapId={'8859a83a13a834f62d11ad10'}
         defaultZoom={3}
         defaultCenter={{ lat: 22.54992, lng: 0 }}
         gestureHandling={'greedy'}
