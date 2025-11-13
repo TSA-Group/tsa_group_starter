ee6d5262a3407d109cc013860ceaa214b84768ee
stopblocking80 <126114544+stopblocking80@users.noreply.github.com>
2025-11-12 18:49:48 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import React from 'react';
 import Link from "next/link";
-import Header from "./components/Header";
+import Header from "../components/Header";
 export default function Home() {
   return (
     <>
