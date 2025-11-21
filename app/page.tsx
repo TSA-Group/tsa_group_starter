530ae879d4c356dd2925d37cac9c90fdfd9339ec
Shivvesh <71574601+Shivvesh@users.noreply.github.com>
2025-11-21 17:19:36 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
       <motion.div
         initial={{ y: -100, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.8, type: "spring", stiffness: 120 }}
+        transition={{ duration: 0.8, delay: 0.25, type: "spring", stiffness: 120 }}
         className="fixed top-0 left-0 w-full z-50 bg-black"
       >
         <Header />
