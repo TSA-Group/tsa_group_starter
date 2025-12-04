c6683cea2ce78bdad5a0f877ce0cc7127c97205d
stopblocking80 <126114544+stopblocking80@users.noreply.github.com>
2025-12-03 21:12:52 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,14 @@ module.exports = {
   darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "../components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
         lightgray: "#CFD8DC",
-        darkgray: "#263238",
+        darkgray: "#37474F",
         teal: "#26A69A",
       },
     },
