f5fb2ef2cacb62c11754021956540cf9b6d0f132
stopblocking80 <126114544+stopblocking80@users.noreply.github.com>
2025-12-07 20:57:10 -0600

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,24 @@
+import { useEffect } from "react";
 
+export default function SpeechDemo() {
+  useEffect(() => {
+    const phrase =
+      "Welcome to the contact section, where you can contact some of our finest members of the community";
+    const utter = new SpeechSynthesisUtterance(phrase);
+    window.speechSynthesis.cancel();
+    window.speechSynthesis.speak(utter);
+  }, []);
+
+  return (
+    <div>
+      <h1>Speech Demo</h1>
+      <p>
+        This page will speak a welcome message when it loads using the Web Speech
+        API.
+      </p>
+    </div>
+  );
+}
 
 
 
