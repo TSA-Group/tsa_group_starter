1fe8f126d33d6826ca2e9a3ffbe1ae67b3bdb79e
ak2m0 <emilr928@icloud.com>
2025-12-07 20:55:56 -0600

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,23 +1,4 @@
-  <!doctype html>
-    <html>
-    <head>
-      <meta charset="utf-8" />
-      <title>Speech Demo</title>
-    </head>
-    <body>
-      <script>
-        function speak() {
-          const phrase = "Welcome to the contact section, where you can contact some of our finest members of the community";
-          const utter = new SpeechSynthesisUtterance(phrase);
-          window.speechSynthesis.cancel();
-          window.speechSynthesis.speak(utter);
-        }
 
-        // Speak on page load
-        window.onload = speak;
-      </script>
-    </body>
-    </html>
 
 
 
