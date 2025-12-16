b217995c12821f6f9f18840cf5b701f0f0e1a90f
Jerkyjupiter74 <jerkyjupiter74@gmail.com>
2025-12-16 15:50:39 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
             style={{ fontFamily: "TAN Buster, sans-serif" }}
             variants={cardPop}
             animate={{
-              x: [-40, -20, -40],
+              x: [-80, -60, -80],
               y: [0, -6, 0],
               transition: { duration: 2.5, ease: "easeInOut", },
             }}
