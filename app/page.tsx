5d7dc64d25c7f9deb7e32af2fb997844ceb710eb
Jerkyjupiter74 <jerkyjupiter74@gmail.com>
2026-01-04 22:32:51 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -193,7 +193,7 @@ export default function Home() {
           layout
           variants={cardPop}
           animate={{
-            x: [-20, 0, -20],
+            x: [0, -20, 0],
             y: [0, -6, 0],
             transition: { duration: 2.5, ease: "easeInOut" },
           }}
