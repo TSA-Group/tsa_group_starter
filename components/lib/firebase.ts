525467f48b99621823f9e27d87de56365408745e
Shivvesh <71574601+Shivvesh@users.noreply.github.com>
2026-01-07 21:20:54 -0600

    Refactor Firebase initialization and use environment variables

--- components/lib/firebase.ts
+++ components/lib/firebase.ts
@@ -1,21 +1,15 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBbL-ITW3-NkxttwqdfTN6Wc8V9ljbnDz4",
-  authDomain: "gatherly-49306.firebaseapp.com",
-  projectId: "gatherly-49306",
-  storageBucket: "gatherly-49306.firebasestorage.app",
-  messagingSenderId: "347577816820",
-  appId: "1:347577816820:web:5c52fb48786df3859c70a1",
-  measurementId: "G-T9GKM5XYN6"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
