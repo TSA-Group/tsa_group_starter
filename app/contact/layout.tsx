ed2ce4ca3541d88a92993b9d535a74563450c219
RogueRanger07 <taw.tx@outlook.com>
2026-01-07 19:08:01 -0600

    Create ContactLayout component with metadata

---
+++ app/contact/layout.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Contact Us | Gatherly",
+};
+
+export default function ContactLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
