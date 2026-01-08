e9ff722e30ba8e5d2d621e23decf547564eff942
Jerkyjupiter74 <jerkyjupiter74@gmail.com>
2026-01-07 21:15:53 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -154,7 +154,7 @@ export default function Home() {
         <motion.section className="lg:col-span-1 flex flex-col justify-start mt-48">
           <motion.div
             className="h-80 w-full"
-            style={{ transform: "translateX(2rem)" }} // shift it more to the right
+            style={{ transform: "translateX(4rem)" }} 
             variants={cardPop}
             initial="hidden"
             animate="show"
