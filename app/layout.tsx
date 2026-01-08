47ae6f1c4b7f1c2b6330a318ee4ba525ad70a65b
Jerkyjupiter74 <jerkyjupiter74@gmail.com>
2026-01-07 20:12:35 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/Header";
 import RouteShell from "./route-shell";
+import { RouteTransition } from "./RouteTransitions";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
