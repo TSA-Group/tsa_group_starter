fe556f7c840e4a64c2ece05805f83aed5de39bff
Jerkyjupiter74 <jerkyjupiter74@gmail.com>
2026-01-15 21:32:54 -0600

    Update page.tsx

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -1,20 +1,2 @@
-
 "use client";
-
-import React, { useEffect, useState } from "react";
-import Link from "next/link";
-import {
-  motion,
-  AnimatePresence,
-  type MotionValue,
-  type Variants,
-  useMotionValue,
-  useMotionValueEvent,
-  useReducedMotion,
-  useScroll,
-  useSpring,
-  useTransform,
-  useVelocity,
-} from "framer-motion";
-
 
