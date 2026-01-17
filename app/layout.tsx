b48c0c73d90dbbd7011e3a6c9c8393e29307211a
Shivvesh <71574601+Shivvesh@users.noreply.github.com>
2026-01-16 21:58:49 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/Header";
 import LayoutShell from "./layout-shell";
+import SiteFooter from "@/components/SiteFooter";
+
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
