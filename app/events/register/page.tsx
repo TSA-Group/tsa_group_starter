b8fa55e0ce3ae99f2d2597769da6ae28dd907c24
Shivvesh <71574601+Shivvesh@users.noreply.github.com>
2026-01-23 22:23:26 -0600

    Update page.tsx

--- app/events/register/page.tsx
+++ app/events/register/page.tsx
@@ -1,4 +1,3 @@
-// app/events/register/page.tsx
 import { Suspense } from "react";
 import RegisterClient from "./register-client";
 
@@ -9,7 +8,6 @@ export default function Page() {
     </Suspense>
   );
 }
-
 function Loading() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-[#F6FAFF] via-[#F2F7FF] to-[#EEF5FF] text-slate-900 flex items-center justify-center">
